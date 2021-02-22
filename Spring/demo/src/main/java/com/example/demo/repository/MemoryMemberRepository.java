da129606514005a6b19e45110a6947bc772f6ec5
DESKTOP-Q82D2AR\dongb <dongbin>
2021-02-22 19:30:01 +0900

    서비스개발(테스트), 컴포넌트스캔, 회원등록(조회)

--- Spring/demo/src/main/java/com/example/demo/repository/MemoryMemberRepository.java
+++ Spring/demo/src/main/java/com/example/demo/repository/MemoryMemberRepository.java
@@ -4,6 +4,7 @@ import com.example.demo.domain.Member;
 
 import java.util.*;
 
+//@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long,Member> store = new HashMap<>();
---
+++ Spring/demo/src/main/java/com/example/demo/service/MemberService.java
@@ -0,0 +1,45 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.Member;
+import com.example.demo.repository.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+//@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    //@Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    //회원가입
+    public Long join(Member member){
+        //같은 이름이 있는 중복 회원X
+        //repository 안에 동일 이름이 있어서 반환값이 null이 아니면 괄호 안 action
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    //중복회원 검증
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m ->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    //전체회원 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
