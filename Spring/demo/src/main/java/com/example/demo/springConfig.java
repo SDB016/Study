90199350704890f59bd1b63b0a8b4e39ba8b4edf
SDB016 <dongbin4013@naver.com>
2021-03-04 16:22:07 +0900

    SpringDataJpa

--- Spring/demo/src/main/java/com/example/demo/springConfig.java
+++ Spring/demo/src/main/java/com/example/demo/springConfig.java
@@ -1,39 +1,45 @@
 package com.example.demo;
 
-import com.example.demo.repository.JpaMemberRepository;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-
 @Configuration
 public class springConfig {
-    /*
-    private final DataSource dataSource;
 
-    public springConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }*/
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public springConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /*
+        private final DataSource dataSource;
+
+        public springConfig(DataSource dataSource) {
+            this.dataSource = dataSource;
+        }*/
+    /*
     private EntityManager em;
 
     @Autowired
     public springConfig(EntityManager em) {
         this.em = em;
     }
-
+*/
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        // return new MemoryMemberRepository();
+    //@Bean
+    //public MemberRepository memberRepository() {
+        //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
         //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+        //return new JpaMemberRepository(em);
+    //}
 }
