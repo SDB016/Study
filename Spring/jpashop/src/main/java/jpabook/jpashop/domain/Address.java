beddd04630ec21ab45023ae751b1839e2a4305f9
SDB016 <dongbin4013@naver.com>
2021-03-08 18:01:31 +0900

    임베디드 타입 수정

--- Spring/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ Spring/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 
 import javax.persistence.Embeddable;
 
+//값 타입은 변경 불가능하게 설계해야 한다. @Setter 대신 생성자 초기화
 @Embeddable
 @Getter
 public class Address {
@@ -11,4 +12,13 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address(){
+    } //JPA 스펙상 엔티티나 임베디드 타입은 기본생성자를 설정해야 한다.
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
