7a0c0a05e9f8b275021d4d1ac6d8843231cce55d
SDB016 <dongbin4013@naver.com>
2021-03-08 19:43:28 +0900

    연관관계 편의 메서드 생성

--- Spring/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ Spring/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -34,4 +34,20 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문상태 [ORDER, CANCEL]
+
+    //==연관관계 편의 메서드==//
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void setOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
