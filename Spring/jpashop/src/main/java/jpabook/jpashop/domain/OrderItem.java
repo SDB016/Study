53cd89a0d1b188633bcd938b0b3be91bdc2e563f
SDB016 <dongbin4013@naver.com>
2021-03-12 00:39:53 +0900

    Order, OrderItem 엔티티 개발

--- Spring/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ Spring/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -26,4 +26,34 @@ public class OrderItem {
 
     private int orderPrice; //주문 당시 가격
     private int count; //주문 수량
+
+    //==생성 메서드==//
+    //할인 등 이유로 가격이 바뀔 수 있기 때문에 가격, 갯수를 받음
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setItem(item);
+        orderItem.setCount(count);
+
+        item.removeStockQuantity(count); //재고 빼기
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+
+    /**
+     * 재고 수량 원복
+     */
+    public void cancel() {
+        getItem().addStockQuantity(count);
+    }
+
+    //==조회 로직==//
+
+    /**
+     *주문상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
