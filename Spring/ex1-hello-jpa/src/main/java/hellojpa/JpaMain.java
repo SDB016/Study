cd73fbf2533c3b4ee1115f41fbe8c997808aafd6
SDB016 <dongbin4013@naver.com>
2021-03-15 16:38:56 +0900

    java11버전 pom.xml 수정

--- Spring/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ Spring/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,9 @@
 package hellojpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class JpaMain {
     public static void main(String[] args) {
 
