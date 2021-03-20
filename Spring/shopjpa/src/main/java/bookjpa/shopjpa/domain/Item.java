422cdb6e030d3166d66043f8be1e5a5d6650a42b
SDB016 <dongbin4013@naver.com>
2021-03-20 17:18:41 +0900

    jpashop without springboot

---
+++ Spring/shopjpa/src/main/java/bookjpa/shopjpa/domain/Item.java
@@ -0,0 +1,47 @@
+package bookjpa.shopjpa.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Item {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ITEM_ID")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockquantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStockquantity() {
+        return stockquantity;
+    }
+
+    public void setStockquantity(int stockquantity) {
+        this.stockquantity = stockquantity;
+    }
+}
