113a51c0a35cdb16867d222814684015d70153fd
SDB016 <dongbin4013@naver.com>
2021-03-23 22:22:06 +0900

    프로젝션 여러 값 조회 (타입이 다른 여러 값)

--- Spring/jpql/src/main/java/jpql/JpaMain.java
+++ Spring/jpql/src/main/java/jpql/JpaMain.java
@@ -19,14 +19,27 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            List<Member> resultList = em.createQuery("select m from Member m where m.username =:username", Member.class)
-                    .setParameter("username", "member1")
+            //방법 3
+            List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
                     .getResultList();
 
-            for (Member member1 : resultList) {
-                System.out.println("member1 = " + member1);
+            for (MemberDTO memberDTO : resultList) {
+                System.out.println("memberDTO.getUsername()= " + memberDTO.getUsername() + ", memberDTO.getAge() =" + memberDTO.getAge());
             }
 
+            /* 방법1 
+            List resultList = em.createQuery("select m.age, m.username from Member m").getResultList();
+            Object[] result = (Object[]) resultList.get(0);
+            System.out.println("result[0] = " + result[0]);
+            System.out.println("result[1] = " + result[1]);
+            */
+            
+            /*방법 2
+            List<Object[]> resultList = em.createQuery("select m.age, m.username from Member m", Object[].class).getResultList();
+            Object[] objects = resultList.get(0);
+            System.out.println("objects[0] = " + objects[0]);
+            System.out.println("objects[1] = " + objects[1]);
+            */
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
