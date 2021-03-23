93881eeed4316e409e717745b24786e46d825832
SDB016 <dongbin4013@naver.com>
2021-03-23 19:13:21 +0900

    parameter binding

---
+++ Spring/jpql/src/main/java/jpql/Address.java
@@ -0,0 +1,35 @@
+package jpql;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+    private String city;
+    private String zipcode;
+    private String street;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+}
---
+++ Spring/jpql/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,38 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            List<Member> resultList = em.createQuery("select m from Member m where m.username =:username", Member.class)
+                    .setParameter("username", "member1")
+                    .getResultList();
+
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
