c18dc05cdcd5d9f26a683e0b3f3cf9728c051f4a
SDB016 <dongbin4013@naver.com>
2021-03-25 18:05:15 +0900

    MemberRepository 개발

--- Spring/jpashop_clone/src/main/java/jpabook/jpashop_clone/repository/MemberRepository.java
+++ Spring/jpashop_clone/src/main/java/jpabook/jpashop_clone/repository/MemberRepository.java
@@ -5,20 +5,30 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class MemberRepository {
 
     @PersistenceContext
-    EntityManager em;
+    private EntityManager em;
 
     public Long save(Member member) {
         em.persist(member);
         return member.getId();
     }
 
-    public Member find(Long id) {
+    public Member findOne(Long id) {
         return em.find(Member.class, id);
     }
 
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public List<Member> findMemberByName(String name) {
+         return em.createQuery("select m from Member m where m.name =:name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
 }
