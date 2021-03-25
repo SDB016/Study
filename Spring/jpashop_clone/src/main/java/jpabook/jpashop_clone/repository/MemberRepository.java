5c154561b5e5d19363c13fde16ce7463a7fdc941
SDB016 <dongbin4013@naver.com>
2021-03-25 18:24:11 +0900

    MemberService 개발, @RequiredArgsConstructor 추가

--- Spring/jpashop_clone/src/main/java/jpabook/jpashop_clone/repository/MemberRepository.java
+++ Spring/jpashop_clone/src/main/java/jpabook/jpashop_clone/repository/MemberRepository.java
@@ -1,17 +1,17 @@
 package jpabook.jpashop_clone.repository;
 
 import jpabook.jpashop_clone.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public Long save(Member member) {
         em.persist(member);
---
+++ Spring/jpashop_clone/src/main/java/jpabook/jpashop_clone/service/MemberService.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop_clone.service;
+
+import jpabook.jpashop_clone.domain.Member;
+import jpabook.jpashop_clone.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor //생성자 인젝션 (final 변수만)
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     *회원가입
+     */
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicatedMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicatedMember(Member member) {
+
+        List<Member> members = memberRepository.findMemberByName(member.getName());
+        if (members.size() != 0){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     *회원 전체 조회
+     */
+    public List<Member> findAll(){
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
