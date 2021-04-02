27aa0e84aef1a313893f33fa3196cf10771b1dc9
SDB016 <dongbin4013@naver.com>
2021-04-02 22:31:24 +0900

    create myBoard Project

---
+++ Spring/myBoard/src/main/java/com/firstHomePage/myBoard/MyBoardApplication.java
@@ -0,0 +1,13 @@
+package com.firstHomePage.myBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBoardApplication.class, args);
+	}
+
+}
