4766aed6a934524e06ef8cdfd26806c2a6eacbee
SDB016 <dongbin4013@naver.com>
2021-05-15 00:11:24 +0900

    회원가입 컨트롤러 생성 & SecurityConfig 예외 처리

---
+++ Spring/dbstudy/src/main/java/com/dbstudy/account/AccountController.java
@@ -0,0 +1,14 @@
+package com.dbstudy.account;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccountController {
+
+    @GetMapping("/sign-up")
+    public String signUpForm(Model model) {
+        return "account/sign-up";
+    }
+}
